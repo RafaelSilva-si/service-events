535917a6e58dcc31b76b9cedca096844ed442753
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-25 16:03:12 -0300

    feat: deve retornar erro se campos obrigatorios estiverem incompletos

--- src/services/add-events.service.ts
+++ src/services/add-events.service.ts
@@ -1,6 +1,7 @@
 import Event from '../domain/models/Event';
 import { AddEvent, AddEventModel } from '../domain/usecases/add-event';
 import EventsRepository from '../repositories/events.repository';
+import { MissingParamError } from '../utils/errors/missingParamError';
 
 class AddEventsService implements AddEvent {
   private readonly eventsRepository: EventsRepository;
@@ -10,6 +11,19 @@ class AddEventsService implements AddEvent {
   }
 
   async add(data: AddEventModel): Promise<Event> {
+    const requiredFields = [
+      'title',
+      'date',
+      'location',
+      'capacity',
+      'category',
+    ];
+
+    for (const field of requiredFields) {
+      if (!data[field]) {
+        throw new MissingParamError(field, 401);
+      }
+    }
     return await this.eventsRepository.add(data);
   }
 }
