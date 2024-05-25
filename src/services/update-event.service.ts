859c358073f1b9b358f77ceccb6433cf2909c571
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-25 17:56:22 -0300

    feat: Deve retornar um erro se o evento não existir com base no ID

--- src/services/update-event.service.ts
+++ src/services/update-event.service.ts
@@ -1,6 +1,7 @@
 import Event from '../domain/models/Event';
 import { UpdateEvent, UpdateEventModel } from '../domain/usecases/update-event';
 import EventsRepository from '../repositories/events.repository';
+import { InvalidParamError } from '../utils/errors/invalidParamError';
 
 class UpdateEventService implements UpdateEvent {
   private readonly eventRepository: EventsRepository;
@@ -10,6 +11,9 @@ class UpdateEventService implements UpdateEvent {
   }
 
   async update(id: string, data: UpdateEventModel): Promise<Event> {
+    const existEvent = await this.eventRepository.getEventByID(id);
+    if (!existEvent) throw new InvalidParamError('Evento Não Existe!', 409);
+
     return await this.eventRepository.update(id, data);
   }
 }
