174af37e5e8c66b4403f3a21ed4507efef53bc9f
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-25 17:28:03 -0300

    feat: cria update de eventos em repository

--- src/repositories/events.repository.ts
+++ src/repositories/events.repository.ts
@@ -1,9 +1,14 @@
 import Event from '../domain/models/Event';
 import { AddEvent, AddEventModel } from '../domain/usecases/add-event';
+import { UpdateEvent, UpdateEventModel } from '../domain/usecases/update-event';
 
-class EventsRepository implements AddEvent {
+class EventsRepository implements AddEvent, UpdateEvent {
   async add(data: AddEventModel): Promise<Event> {
     return { ...data, id: '1' };
+  }
+
+  async update(id: string, data: UpdateEventModel): Promise<Event> {
+    return { id, ...data };
   }
 }
 
