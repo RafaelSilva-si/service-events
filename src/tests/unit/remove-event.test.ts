779e7f30da88dd15486e11de1d1ee7add6f48c7d
Rafael Mariano da Silva <rafaelmarianodasilva@MacBook-Air-de-Rafael.local>
2024-05-26 13:29:07 -0300

    test: remove erro se tentar remover evento não existente

--- src/tests/unit/remove-event.test.ts
+++ src/tests/unit/remove-event.test.ts
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import EventsRepository from '../../repositories/events.repository';
 import RemoveEventService from '../../services/remove-event.service';
+import Sinon from 'sinon';
 
 const mocks = {
   returnSuccess: require('../mocks/return-success-event.json'),
@@ -10,8 +11,23 @@ describe('Remove Events', () => {
   const eventRepository = new EventsRepository();
   const removeEventService = new RemoveEventService(eventRepository);
 
+  afterEach(() => {
+    Sinon.restore();
+  });
+
   it('Deve remover um evento com base em seu ID', async () => {
     const result = await removeEventService.remove('1');
     assert.deepEqual(result, mocks.returnSuccess);
+  });
+
+  it('Deve retornar um erro, caso o evento não exista', async () => {
+    Sinon.stub(EventsRepository.prototype, 'getEventByID').resolves(false);
+
+    const promise = removeEventService.remove('2');
+    await assert.rejects(promise, (error: any) => {
+      assert.deepEqual(error.status, 409);
+      assert.deepEqual(error.message, 'Missing param Esse evento não existe');
+      return true;
+    });
   });
 });
